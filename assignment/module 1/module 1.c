008127e39d4f7e9eda31f9858075c4f1e69cc09c
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2022-12-14 14:54:43 +0530

    Add files via upload

---
+++ assignment/module 1/module 1.c
@@ -0,0 +1,11 @@
+//modul 1(c leguage introduction) 
+#include <stdio.h>
+void main ()
+{
+    printf("my name is Sagar\n");
+    printf("my birth date 22 August 2002\n");
+    printf("my age 20\n");
+    printf("my address:-bhaktinagar station plot2/18,360002");
+}
+
+
