abc29654d03eb7d62ef8293ff00bb49f9dd8a6e6
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-01-04 21:06:53 +0530

    Add files via upload

---
+++ Task/convert week into year.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{   int week;
+	float year;
+	printf("enter the number of weeks : ");
+	scanf("%d", &week);
+	year = week/52;
+	printf("the number of years is :%f",year);
+	
+}
