64501cd4ad22024a7624550955b35312161e3d74
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-01-04 20:56:53 +0530

    Add files via upload

---
+++ Task/convert .c
@@ -0,0 +1,12 @@
+//wap to convert day into year
+#include<stdio.h>
+int main()
+{
+	float week;
+    int day;
+	printf("enter the number of days  ");
+	scanf("%d", &day);
+	week = day/7;
+	printf("the number of week is :%f",week);
+	
+}
