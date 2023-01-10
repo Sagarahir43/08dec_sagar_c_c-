76591e4205a78337cd8698cf675bf4e22e80a225
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-01-10 21:35:14 +0530

    Add files via upload

---
+++ Task/result.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	float eng,guj,com,total,percentage;
+	printf("enter marks of eng between 0-100 : ");
+	scanf("%f",&eng);
+	printf("enter marks of guj between 0-100 : ");
+	scanf("%f",&guj);
+	printf("enter marks of com between 0-100 : ");
+	scanf("%f", &com);
+	if(eng>=33&&guj&&33&&com>=33)
+	{printf("student is pass\n");
+
+	total = guj+eng+com;
+	printf("total is =%f\n", total);
+	
+	percentage= total/3;
+	printf("percentage =%f\n",percentage);
+
+	if(percentage>=33&&percentage<60)
+	          {printf("pass class \n");}
+	 else if(percentage<=80&&percentage>60)
+	               {printf("second class\n");}
+        	else if(percentage<=100&&percentage>80)
+	                  {printf("first class \n");}
+	
+	}
+	else{printf("fail");
+	}
+
+
+
+	return 0;
+}
