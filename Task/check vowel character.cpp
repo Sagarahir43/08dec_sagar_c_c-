a276f12d1284fbca569a696d81b9cdda72c76b95
Sagarahir43 <120082667+Sagarahir43@users.noreply.github.com>
2023-01-16 08:42:49 +0530

    Add files via upload

---
+++ Task/check vowel character.cpp
@@ -0,0 +1,46 @@
+//wqp check vowel or consonant 
+#include<stdio.h>
+int main()
+{
+	
+	char ch;
+	printf("enter alphabet character : ");
+	scanf("%c",&ch);
+	
+	switch(ch)
+    {
+    	case 'a':
+    		printf("Enter character is Vowel");
+    		break;
+    	case 'e':
+		    printf("Enter character is Vowel");
+			break;
+		case 'i':
+		    printf("Enter character is Vowel");
+			break;
+		case 'o':
+		    printf("Enter character is Vowel");
+			break;
+		case 'u':
+		    printf("Enter character is Vowel");
+			break;
+		case 'A':
+		    printf("Enter character is Vowel");
+			break;
+		case 'E':
+		    printf("Enter character is Vowel");
+			break;
+		case 'I':
+			printf("Enter character is Vowel");
+			break;
+		case 'O':
+			printf("Enter character is Vowel");
+			break;
+		case 'U':
+			printf("Enter character is Vowel");
+			break;
+		default:
+			printf("Enter character is Consonant");					
+	}
+	   
+}
